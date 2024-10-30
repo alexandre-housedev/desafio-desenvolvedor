cbcfdc7c1df0982d300676a8139e9060a322a764
DESKTOP-RAIKHC2\katsu <alexandre@agenciainnovai.com.br>
2024-10-29 21:52:06 -0300

    feat: Consulta de histórico com autenticação

--- desafio/routes/api.php
+++ desafio/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\UploadFileController;
+use App\Http\Controllers\Api\HistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,5 @@ Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::post('/upload-file', [UploadFileController::class, 'upload'])->name('upload');
+    Route::get('/history', [HistoryController::class,'history_upload'])->name('history');
 });
