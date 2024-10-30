ee425db203bdf7afceb257f53423fde5ad127074
DESKTOP-RAIKHC2\katsu <alexandre@agenciainnovai.com.br>
2024-10-29 21:47:58 -0300

    feat: Upload de arquivo com autenticação de rota

--- desafio/routes/api.php
+++ desafio/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\UploadFileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/upload-file', [UploadFileController::class, 'upload'])->name('upload');
+});
