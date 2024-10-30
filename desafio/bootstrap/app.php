c2acec687b11d949126955989b5c8640a0a4b8dd
DESKTOP-RAIKHC2\katsu <alexandre@agenciainnovai.com.br>
2024-10-29 21:36:21 -0300

    feat: Criação do Usuário e autenticação via token da api

--- desafio/bootstrap/app.php
+++ desafio/bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function(AuthenticationException $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Token de autenticação Inválido'
+            ]);
+        });
     })->create();
